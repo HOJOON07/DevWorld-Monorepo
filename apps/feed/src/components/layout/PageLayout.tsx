499db4365c489bda08f9f06c32bdaa5098b85ac1
hojoon07 <ghwns107@naver.com>
2025-07-14 17:05:08 +0900

    feed layout잡기

--- apps/feed/src/components/layout/PageLayout.tsx
+++ apps/feed/src/components/layout/PageLayout.tsx
@@ -6,7 +6,7 @@ export default function PageLayout() {
   return (
     <div className='relative min-h-svh justify-between bg-gray-50'>
       <IconSidebar />
-      <div className='ml-20 flex py-5 pr-5'>
+      <div className='ml-20 flex pr-1'>
         <main className='flex-1'>
           <div className='mx-auto max-w-[640px]'>
             <FeedCard />
