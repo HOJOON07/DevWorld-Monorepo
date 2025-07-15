29c5f2a1dcafd0052f5c24899ac647bc757534be
hojoon07 <ghwns107@naver.com>
2025-07-16 03:37:30 +0900

    docs: dev docs routing config 작성:

--- dev-docs/src/content/Front-End/_meta.js
+++ dev-docs/src/content/Front-End/_meta.js
@@ -9,6 +9,7 @@ const meta = {
   'oauth-dynamic-routing': '',
   'react-hook-form-context': '',
   'shell-router-architecture': '',
+  'shell-routing-root-redirect': '',
   'sidebar-compound-component': '',
   'tanstack-query-mfa': '',
 };
